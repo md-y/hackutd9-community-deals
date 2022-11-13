c6f80a64c59b2cde13a87980570ed9b86f4bf1ce
md-y <15069105+md-y@users.noreply.github.com>
2022-11-12 23:56:29 -0600

    Add nessie

--- lib/screens/friends.dart
+++ lib/screens/friends.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hackutd9/services/nessie.dart';
 import 'package:hackutd9/services/user.dart';
 
 class Friends extends StatefulWidget {
@@ -11,20 +12,10 @@ class Friends extends StatefulWidget {
 }
 
 class _FriendsState extends State<Friends> {
-  final Future<List<String>> _namesFuture =
-      FirebaseFirestore.instance.collection('deals').get().then(
-    (res) {
-      var docs = res.docs;
-      List<String> names = [];
-      for (var doc in docs) {
-        var data = doc.data();
-        var name = data['posterName'];
-        if (!names.contains(name) && name != User.username) {
-          names.add(name);
-        }
-      }
-      return names;
-    },
+  final Future<List<String>> _namesFuture = Nessie.getAllCustomers().then(
+    (res) => res.map((customer) {
+      return '${customer['first_name']} ${customer['last_name']}';
+    }).toList(),
   );
 
   @override
