a0dc9dca9d3f9997f7e885f5c7f301fb6a2f5117
md-y <15069105+md-y@users.noreply.github.com>
2022-11-13 09:52:27 -0600

    Add transfer support

--- lib/widgets/friend_floating_button.dart
+++ lib/widgets/friend_floating_button.dart
@@ -1,13 +1,52 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:hackutd9/services/nessie.dart';
+import 'package:hackutd9/services/user.dart';
 
 class FriendFloatingButton extends StatelessWidget {
+  const FriendFloatingButton({super.key});
+
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
-      child: const Icon(Icons.add),
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (ctx) {
+            return FutureBuilder(
+              future: Nessie.getAllTansfers(User.account),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return AlertDialog(
+                    content: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.8,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: ListView(
+                        children: snapshot.data!.map((transfer) {
+                          return ListTile(
+                            title: Text(transfer['amount'].toString()),
+                            subtitle: Text(transfer['status']),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        child: const Text('Exit'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      )
+                    ],
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            );
+          },
+        );
+      },
+      child: const Icon(Icons.info),
     );
   }
 }
