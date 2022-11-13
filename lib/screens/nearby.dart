d2cd7f7ba308d478b80f759771700d1cb4c16440
md-y <15069105+md-y@users.noreply.github.com>
2022-11-13 02:04:15 -0600

    Add pin popups

--- lib/screens/nearby.dart
+++ lib/screens/nearby.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hackutd9/services/deal.dart';
+import 'package:hackutd9/widgets/card_widget.dart';
 
 class Nearby extends StatefulWidget {
   const Nearby({super.key});
@@ -26,14 +27,39 @@ class _NearbyState extends State<Nearby> {
             )
         .then((deals) {
       setState(() {
-        _markers = Set.from(deals.map((deal) {
-          print(true);
-          return Marker(
-            markerId: MarkerId(deal.id),
-            position: LatLng(deal.location.latitude, deal.location.longitude),
-            visible: true,
-          );
-        }));
+        _markers = Set.from(
+          deals.map((deal) {
+            return Marker(
+              markerId: MarkerId(deal.id),
+              position: LatLng(deal.location.latitude, deal.location.longitude),
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        contentPadding: EdgeInsets.zero,
+                        content: SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.height * 0.15,
+                          child: Stack(children: [
+                            Positioned(
+                              left: MediaQuery.of(context).size.width * -0.1,
+                              child: CardWidget(deal: deal),
+                            )
+                          ]),
+                        ),
+                        actions: [
+                          TextButton(
+                            child: const Text('Exit'),
+                            onPressed: () => Navigator.of(context).pop(),
+                          )
+                        ],
+                      );
+                    });
+              },
+            );
+          }),
+        );
       });
     });
   }
@@ -44,40 +70,38 @@ class _NearbyState extends State<Nearby> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Nearby Deals'),
-          centerTitle: true,
-          titleTextStyle: TextStyle(fontSize: 25),
-          backgroundColor: Colors.blue[600],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nearby Deals'),
+        centerTitle: true,
+        titleTextStyle: TextStyle(fontSize: 25),
+        backgroundColor: Colors.blue[600],
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-          markers: _markers,
-        ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-          child: FloatingActionButton(
-            backgroundColor: Theme.of(context).primaryColor,
-            foregroundColor: Colors.black,
-            onPressed: () => mapController.animateCamera(
-              CameraUpdate.newCameraPosition(
-                CameraPosition(
-                  target: _center,
-                  zoom: 11.0,
-                ),
+        markers: _markers,
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+        child: FloatingActionButton(
+          backgroundColor: Theme.of(context).primaryColor,
+          foregroundColor: Colors.black,
+          onPressed: () => mapController.animateCamera(
+            CameraUpdate.newCameraPosition(
+              CameraPosition(
+                target: _center,
+                zoom: 11.0,
               ),
             ),
-            child: const Icon(Icons.center_focus_strong),
           ),
+          child: const Icon(Icons.center_focus_strong),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
     );
   }
 }
