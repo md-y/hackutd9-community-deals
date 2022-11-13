dcc82c7f37d7ae93a25379162c04fe12fc5f2cac
md-y <15069105+md-y@users.noreply.github.com>
2022-11-12 23:55:05 -0600

    Add pins to nearby map

--- lib/screens/nearby.dart
+++ lib/screens/nearby.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hackutd9/services/deal.dart';
 
 class Nearby extends StatefulWidget {
   const Nearby({super.key});
@@ -13,6 +14,29 @@ class _NearbyState extends State<Nearby> {
   late GoogleMapController mapController;
 
   final LatLng _center = const LatLng(32.9857, -96.7502);
+  // final int _radius = 20;
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    Deal.getDeals(
+            // centerLoc: GeoPoint(_center.latitude, _center.longitude),
+            // radius: _radius,
+            )
+        .then((deals) {
+      setState(() {
+        _markers = Set.from(deals.map((deal) {
+          print(true);
+          return Marker(
+            markerId: MarkerId(deal.id),
+            position: LatLng(deal.location.latitude, deal.location.longitude),
+            visible: true,
+          );
+        }));
+      });
+    });
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -32,6 +56,7 @@ class _NearbyState extends State<Nearby> {
             target: _center,
             zoom: 11.0,
           ),
+          markers: _markers,
         ),
         floatingActionButton: Padding(
           padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
